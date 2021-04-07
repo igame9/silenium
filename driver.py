18e15854feb5b71c25bb3ce0233726bffbb819a8
igame9 <andreygrigorev0@gmail.com>
2021-04-07 21:47:35 +0300

    remake with threads

---
+++ driver.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+
+def initDriverChrome():
+    chromedriver = r"C:\Users\Andrey\PycharmProjects\silenium\chromedriver.exe"
+    options = webdriver.ChromeOptions()
+    # options.add_argument('headless')  # для открытия headless-браузера
+    driver = webdriver.Chrome(executable_path=chromedriver, options=options)
+    return driver
